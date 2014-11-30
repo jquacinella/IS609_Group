be7e7a210db1307a14a659d56d930ed3dd14e636
ajp619 <ajp619@outlook.com>
2014-11-30 11:57:20 -0800

    twitter_crawl code complete. Next step is to run crawl to populate links.

--- GraphTheory/twitter_crawl.py
+++ GraphTheory/twitter_crawl.py
@@ -6,11 +6,18 @@ __author__ = 'Aaron'
 
 import twitter
 import cPickle as pickle
+import os
+from time import sleep
 
 
 def saveobject(obj, filename):
     """Shortcut to save file"""
     # import cPickle as pickle
+    # Save in object directory
+    target_dir = os.path.basename(__file__).split('.')[0] + '_objects'
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    filename = os.path.join(target_dir, filename)
     with open(filename, 'wb') as output:
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
@@ -18,6 +25,8 @@ def saveobject(obj, filename):
 def readobject(filename):
     """Shortcut to read file"""
     # import cPickle as pickle
+    target_dir = os.path.basename(__file__).split('.')[0] + '_objects'
+    filename = os.path.join(target_dir, filename)
     with open(filename, 'rb') as input_file:
         return pickle.load(input_file)
 
@@ -32,46 +41,28 @@ def authenticate():
     return api
 
 
+def get_to_crawl(api):
+    try:
+        return readobject("to_crawl")
+    except IOError:
+        # Get community kernel
+        scientists = data_scientist_names()
+
+        # Get user_ids for community kernel
+        return provide_user_seeds(scientists, api)
+
+
 def data_scientist_names():
     # Provide a seed list for the data science community
     # Not directly providing twitter ids in case we want
     # to look those up in the future
 
-    # return ["Nate Silver",
-    #         "Mike Bostock",
-    #         "DJ Patil",
-    #         "Hilary Mason",
-    #         "Drew Conway",
-    #         "Hadley Wickham"]
-
-    # Start with this
-    return ["Hilary Mason"]
-
-
-def add_to_user_dictionary(user_id, user_dict, api):
-    if user_id in user_dict.keys():
-        # TODO: Fix counter ...
-        # This doesn't work right if working from copy
-        # Saved to Disk
-        user_dict[user_id]['count'] += 1
-    else:
-        user_dict[user_id] = {
-            'count': 1,
-            'twit_ob': api.GetUser(user_id=user_id)
-        }
-
-
-def add_to_ds_community(user_id, ds_community, api):
-    if user_id in ds_community.keys():
-        # TODO: Fix counter ...
-        # This doesn't work right if working from copy
-        # Saved to Disk
-        ds_community[user_id]['count'] += 1
-    else:
-        ds_community[user_id] = {
-            'count': 1,
-            'follows': api.GetFriendIDs(user_id=user_id)
-        }
+    return ["Nate Silver",
+            "Mike Bostock",
+            "DJ Patil",
+            "Hilary Mason",
+            "Drew Conway",
+            "Hadley Wickham"]
 
 
 def provide_user_seeds(names, api):
@@ -84,6 +75,26 @@ def provide_user_seeds(names, api):
                   "Hadley Wickham": "hadleywickham"}
     # TODO: add case checking. e.g. hilary matches HilArY
     return [api.GetUser(screen_name=translator[n]).id for n in names]
+
+
+def add_to_user_dictionary(user_id, user_dict, user):
+    if user_id in user_dict.keys():
+        user_dict[user_id]['count'] += 1
+    else:
+        user_dict[user_id] = {
+            'count': 1,
+            'twit_ob': user
+        }
+
+
+def add_to_ds_community(user_id, ds_community, follows):
+    if user_id in ds_community.keys():
+        ds_community[user_id]['count'] += 1
+    else:
+        ds_community[user_id] = {
+            'count': 1,
+            'follows': follows
+        }
 
 
 def get_user_dict():
@@ -107,9 +118,13 @@ def get_id_errors():
         return []
 
 
-def crawl_ds_community(user_seeds, ds_community, user_dict, id_errors, api):
-    # Initiate list to be crawled
-    to_crawl = user_seeds
+def extend_to_crawl(to_crawl, user_id, ds_community):
+    to_crawl.extend(
+        [uid for uid in ds_community[user_id]['follows'] if uid not in ds_community.keys()]
+    )
+
+
+def crawl_ds_community(to_crawl, ds_community, user_dict, id_errors, api):
 
     counter = 0
     twitter_errors = 0
@@ -118,19 +133,31 @@ def crawl_ds_community(user_seeds, ds_community, user_dict, id_errors, api):
 
         if user_id not in id_errors:
             try:
-                add_to_user_dictionary(user_id, user_dict, api)
-                add_to_ds_community(user_id, ds_community, api)
-                to_crawl = to_crawl + \
-                           [id for id in ds_community[user_id]['follows'] if id not in ds_community.keys()]
-            except twitter.TwitterError:
-                if user_id in user_dict.keys():
-                    del user_dict[user_id]
-                id_errors.append(user_id)
-                twitter_errors += 1
+                if user_id not in user_dict.keys():
+                    user = api.GetUser(user_id=user_id)
+                else:
+                    user = user_dict[user_id]['twit_ob']
 
-        if counter >= 50:
-            saveobject(ds_community, "ds_community")
-            saveobject(user_dict, "user_dict")
+                if user_id not in ds_community.keys():
+                    follows = api.GetFriendIDs(user_id=user_id)
+                else:
+                    follows = ds_community[user_id]['follows']
+
+            except twitter.TwitterError as error_text:
+                if error_text[0][0]['code'] == 88:
+                    # Exceeded limits (15/15 minutes):
+                    # Add user_id back onto to_crawl and wait
+                    to_crawl.append(user_id)
+                    sleep(60*5)     # seconds to pause
+                else:
+                    id_errors.append(user_id)
+                    twitter_errors += 1
+            else:
+                add_to_user_dictionary(user_id, user_dict, user)
+                add_to_ds_community(user_id, ds_community, follows)
+                extend_to_crawl(to_crawl, user_id, ds_community)
+
+        if counter >= 5:
             print "there were {} errors in getting twitter followers".format(twitter_errors)
             print "there were {} more user ids in the to_crawl list".format(len(to_crawl))
             break
@@ -142,11 +169,8 @@ def main():
     # Authenticate
     api = authenticate()
 
-    # Get community kernel
-    scientists = data_scientist_names()
-
-    # Get user_ids for community kernel
-    user_seeds = provide_user_seeds(scientists, api)
+    # Load or initialize to_crawl list
+    to_crawl = get_to_crawl(api)
 
     # Load or initialize dictionary of user information
     user_dict = get_user_dict()
@@ -158,16 +182,14 @@ def main():
     id_errors = get_id_errors()
 
     # Crawl twitter for structure
-    crawl_ds_community(user_seeds,
+    crawl_ds_community(to_crawl,
                        ds_community, user_dict,
                        id_errors,
                        api)
 
-    print ds_community
-    print user_dict
-
     saveobject(ds_community, "ds_community")
     saveobject(user_dict, "user_dict")
+    saveobject(to_crawl, "to_crawl")
 
 
 if __name__ == "__main__":
