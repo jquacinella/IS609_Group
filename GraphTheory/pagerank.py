92bab3c0490e00ceae0a4aca46d5b2f9e2a7c04b
James Quacinella <james@deemok.com>
2014-12-12 21:48:24 -0500

    Adding comments to pagerank code

--- GraphTheory/pagerank.py
+++ GraphTheory/pagerank.py
@@ -2,11 +2,17 @@ import pickle
 import networkx as nx
 import operator
 
+# Load the crawled graph object
 g = pickle.load(open("graph_object", "r"))
 user_dict = pickle.load(open("twitter_crawl_objects/user_name", "r"))
 
+# Run Network's PageRank algorithm to find best nodes
 pr = nx.pagerank(g, alpha=0.9)
+
+# Sort nodes based on PageRank score
 sorted_pr = sorted(pr.items(), key=operator.itemgetter(1))
 
+# Print out top 20 nodes
+# NOTE: highly similar to thedegree centrality results
 for node in sorted_pr[-20:]:
     print node[1], user_dict[node[0]]['name']
